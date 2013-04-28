9b888619a87ae527a336e4b64121905975079ef3
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-04-28 19:20:33 -0300

    Refactor articles.models, based Container (Content Type)

--- opps/articles/models.py
+++ opps/articles/models.py
@@ -8,13 +8,15 @@ from django.contrib.redirects.models import Redirect
 from taggit.managers import TaggableManager
 from googl.short import GooglUrlShort
 
-from opps.core.models import Publishable, BaseBox, BaseConfig
-from opps.core.models import Slugged
+from opps.core.models import BaseConfig
+from opps.core.models import Imaged, Container
 
 
-class Article(Publishable, Slugged):
-    title = models.CharField(_(u"Title"), max_length=140, db_index=True)
-    headline = models.TextField(_(u"Headline"), blank=True)
+class Article(Imaged):
+
+    __metaclass__ = Container
+
+    headline = models.TextField(_(u"Headline"), null=True, blank=True)
     short_title = models.CharField(
         _(u"Short title"),
         max_length=140,
@@ -24,40 +26,6 @@ class Article(Publishable, Slugged):
         _("Short URL"),
         null=True, blank=False,
     )
-    channel = models.ForeignKey(
-        'channels.Channel',
-        verbose_name=_(u"Channel"),
-    )
-    channel_name = models.CharField(
-        _(u"Channel name"),
-        max_length=140,
-        null=True, blank=False,
-        db_index=True,
-    )
-    channel_long_slug = models.CharField(
-        _(u"Channel long slug"),
-        max_length=250,
-        null=True, blank=False,
-        db_index=True,
-    )
-    child_class = models.CharField(
-        _(u'Child class'),
-        max_length=30,
-        null=True, blank=False,
-        db_index=True
-    )
-    main_image = models.ForeignKey(
-        'images.Image',
-        null=True, blank=False,
-        on_delete=models.SET_NULL,
-        verbose_name=_(u'Main Image'),
-    )
-    images = models.ManyToManyField(
-        'images.Image',
-        null=True, blank=True,
-        related_name='article_images',
-        through='articles.ArticleImage',
-    )
     sources = models.ManyToManyField(
         'sources.Source',
         null=True, blank=True,
@@ -65,31 +33,19 @@ class Article(Publishable, Slugged):
     )
     tags = TaggableManager(blank=True)
 
+    class Meta:
+        abstract = True
+
     def __unicode__(self):
         return self.get_absolute_url()
-
-    class Meta:
-        ordering = ['-date_available', ]
 
     def save(self, *args, **kwargs):
         if not self.short_url:
             self.short_url = GooglUrlShort(self.get_http_absolute_url())\
                 .short()
-        self.channel_name = self.channel.name
-        self.channel_long_slug = self.channel.long_slug
-        self.child_class = self.__class__.__name__
-        super(Article, self).save(*args, **kwargs)
 
     def get_absolute_url(self):
         return "/{0}/{1}".format(self.channel.long_slug, self.slug)
-
-    def get_thumb(self):
-        return self.main_image
-
-    @property
-    def search_category(self):
-        """for use in search result"""
-        return _(self.child_class)
 
     def get_http_absolute_url(self):
         return "http://{0}/{1}".format(self.channel, self.slug)
@@ -98,12 +54,6 @@ class Article(Publishable, Slugged):
     def recommendation(self):
         return [a for a in Article.objects.filter(
             tags__in=self.tags.all()).exclude(pk=self.pk)[:10]]
-
-    def all_images(self):
-        imgs = [i for i in self.images.filter(
-            published=True, date_available__lte=timezone.now())]
-
-        return list(set(imgs))
 
 
 class Post(Article):
@@ -163,105 +113,6 @@ class Link(Article):
         obj.new_path = self.url
         obj.save()
         super(Link, self).save(*args, **kwargs)
-
-
-class ArticleSource(models.Model):
-    article = models.ForeignKey(
-        'articles.Article',
-        null=True, blank=True,
-        on_delete=models.SET_NULL,
-        related_name='articlesource_articles',
-        verbose_name=_(u'Article'),
-    )
-    source = models.ForeignKey(
-        'sources.Source',
-        null=True, blank=True,
-        on_delete=models.SET_NULL,
-        related_name='articlesource_sources',
-        verbose_name=_(u'Source'),
-    )
-    order = models.PositiveIntegerField(_(u'Order'), default=0)
-
-    def __unicode__(self):
-        return self.source.slug
-
-
-class ArticleImage(models.Model):
-    article = models.ForeignKey(
-        'articles.Article',
-        verbose_name=_(u'Article'),
-        null=True, blank=True,
-        related_name='articleimage_articles',
-        on_delete=models.SET_NULL
-    )
-    image = models.ForeignKey(
-        'images.Image',
-        verbose_name=_(u'Image'),
-        null=True, blank=True,
-        on_delete=models.SET_NULL
-    )
-    order = models.PositiveIntegerField(_(u'Order'), default=0)
-
-    def __unicode__(self):
-        return self.image.title
-
-
-class ArticleBox(BaseBox):
-
-    articles = models.ManyToManyField(
-        'articles.Article',
-        null=True, blank=True,
-        related_name='articlebox_articles',
-        through='articles.ArticleBoxArticles'
-    )
-    queryset = models.ForeignKey(
-        'boxes.QuerySet',
-        null=True, blank=True,
-        verbose_name=_(u'Query Set')
-    )
-
-    def get_queryset(self):
-        _app, _model = self.queryset.model.split('.')
-        model = models.get_model(_app, _model)
-
-        queryset = model.objects.filter(published=True,
-                                        date_available__lte=timezone.now())
-        if self.queryset.channel:
-            queryset = queryset.filter(channel=self.queryset.channel)
-        queryset = queryset.order_by('{0}id'.format(self.queryset.order))[
-            :self.queryset.limit]
-
-        return queryset
-
-
-class ArticleBoxArticles(models.Model):
-    articlebox = models.ForeignKey(
-        'articles.ArticleBox',
-        null=True, blank=True,
-        on_delete=models.SET_NULL,
-        related_name='articleboxarticles_articleboxes',
-        verbose_name=_(u'Article Box'),
-    )
-    article = models.ForeignKey(
-        'articles.Article',
-        null=True, blank=True,
-        on_delete=models.SET_NULL,
-        related_name='articleboxarticles_articles',
-        verbose_name=_(u'Article'),
-    )
-    order = models.PositiveIntegerField(_(u'Order'), default=0)
-
-    def __unicode__(self):
-        return u"{0}-{1}".format(self.articlebox.slug, self.article.slug)
-
-    def clean(self):
-
-        if not self.article.published:
-            raise ValidationError(_(u'Article not published!'))
-
-        if self.article.date_available >= timezone.now():
-            raise ValidationError(_(u'Article date available is greater than '
-                                    u'today!'))
 
 
 class ArticleConfig(BaseConfig):
